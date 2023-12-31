7f2f4577995f433f4ab64c1983deacb55ca82a25
benjamin read <readb5@cardiff.ac.uk>
2023-12-31 08:54:15 +0000

    served static html file

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,39 @@
-#[macro_use] extern crate rocket;
+
+use rocket::{get, launch, routes, uri};
+use rocket::response::Redirect;
+use rocket::fs::NamedFile;
+// use rocket_contrib::serve::StaticFiles;
+
+use std::path::Path;
+use std::io;
 
 #[get("/")]
-fn index() -> &'static str {
-    return "Hello, world!"
+fn index() -> Redirect {
+    return Redirect::to(uri!("/home"))
+}
+
+#[get("/home")]
+async fn home() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/home.html")).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index,home])
 }
 
+// #[macro_use] extern crate rocket;
+
+// use rocket::fs::NamedFile;
+// use std::path::{Path, PathBuf};
+
+// #[get("/home")]
+// async fn home() -> Option<NamedFile> {
+//     NamedFile::open(Path::new("static/home.html")).await.ok()
+// }
+
+// #[launch]
+// fn rocket() -> _ {
+//     rocket::build().mount("/", routes![home])
+// }
